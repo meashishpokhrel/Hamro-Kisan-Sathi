d4a0b4e27a303fc6390af3bc34612486665e9f47
meashishpokhrel <meashishpokhrel@gmail.com>
2020-09-08 16:52:47 +0545

    Sliding Image done

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import Home from './screen/Home'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const stackNavigator = createStackNavigator({
+  "Hamro Kishan Sathi":Home
+})
+
+const App = createAppContainer(stackNavigator)
+export default App
